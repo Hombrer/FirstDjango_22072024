921aa7aedbb3336b5f1d7964a6297156638c2b28
Vadim Shikhovtsov <5208643@mail.ru>
2024-07-23 14:15:17 +0300

    Done. M2:task1-6 and about template

--- MainApp/views.py
+++ MainApp/views.py
@@ -3,13 +3,7 @@ from django.shortcuts import render
 
 # Create your views here.
 
-author = {
-    "Имя": "Иван",
-    "Отчество": "Петрович",
-    "Фамилия": "Иванов",
-    "телефон": "8-923-600-01-02",
-    "email": "vasya@mail.ru",
-}
+
 
 
 items = [
@@ -30,35 +24,28 @@ def home(request):
 
 
 def about(request):
-    text = f"""
-    Имя: <b>{author["Имя"]}</b><br>
-    Отчество: <b>{author["Отчество"]}</b><br>
-    Фамилия: <b>{author["Фамилия"]}</b><br>
-    телефон: <b>{author["телефон"]}</b><br>
-    email: <b>{author["email"]}</b><br>
-    """
-    return HttpResponse(text)
+    author = {
+        "name": "Иван",
+        "middle_name": "Петрович",
+        "last_name": "Иванов",
+        "phone": "8-923-600-01-02",
+        "email": "vasya@mail.ru",
+    }
+    return render(request, "about.html", {"author": author})
 
 
 def get_item(request, item_id):
     """ По указанному id возвращаем имя и кол-во элемента """
     for item in items:
         if item['id'] == item_id:
-            result = f"""
-            <h2> Имя: {item["name"]} </h2>
-            <p> Количество: {item["quantity"]} </p>
-            <p> <a href="/items"> Назад к списку товаров </a></p>
-            """
-            return HttpResponse(result)
+            context = {
+                "item": item
+            }
+            return render(request, "item_page.html", context)
     return render(request, "errors.html", {"error": f"Item with id={item_id} not found."})
 
 
 def get_items(request):
-    # result = "<h1>Список товаров</h1><ol>"
-    # for item in items:
-    #     result += f"""<li> <a href="/item/{item['id']}"> {item['name']}</li> """
-    # result += "</ol>"
-    # return HttpResponse(result)
     context = {
         "items": items
     }
