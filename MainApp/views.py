adf8af636341224849f4e026e4bd6dc52fcdb5c4
Vadim Shikhovtsov <5208643@mail.ru>
2024-07-24 10:18:12 +0300

    Done. M3:part1 tast3-4

--- MainApp/views.py
+++ MainApp/views.py
@@ -6,17 +6,6 @@ from django.core.exceptions import ObjectDoesNotExist
 # Create your views here.
 
 
-
-
-items = [
-   {"id": 1, "name": "Кроссовки abibas" ,"quantity":5},
-   {"id": 2, "name": "Куртка кожаная" ,"quantity":2},
-   {"id": 5, "name": "Coca-cola 1 литр" ,"quantity":12},
-   {"id": 7, "name": "Картофель фри" ,"quantity":0},
-   {"id": 8, "name": "Кепка" ,"quantity":124},
-]
-
-
 def home(request):
     context = {
         "name": "Петров Иван Николаевич",
@@ -38,13 +27,13 @@ def about(request):
 
 def get_item(request, item_id):
     """ По указанному id возвращаем имя и кол-во элемента """
-    for item in items:
-        if item['id'] == item_id:
-            context = {
-                "item": item
-            }
-            return render(request, "item_page.html", context)
-    return render(request, "errors.html", {"error": f"Item with id={item_id} not found."})
+    try:
+        item = Item.objects.get(id=item_id)
+    except ObjectDoesNotExist:
+        return render(request, "errors.html", {"error": f"Item with id={item_id} not found."})
+    else:
+        context= {"item": item}
+        return render(request, "item_page.html", context)
 
 
 def get_items(request):
